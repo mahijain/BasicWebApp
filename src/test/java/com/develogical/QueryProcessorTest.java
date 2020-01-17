9fe995aef59cbecc2b21b45888f173d4592c624d
Maria Musat <mm2119@imperial.ac.uk>
2020-01-17 18:15:10 +0000

    chnage html

--- src/test/java/com/develogical/QueryProcessorTest.java
+++ src/test/java/com/develogical/QueryProcessorTest.java
@@ -25,4 +25,5 @@ public class QueryProcessorTest {
     public void isNotCaseSensitive() throws Exception {
         assertThat(queryProcessor.process("shakespeare"), containsString("playwright"));
     }
+    
 }
